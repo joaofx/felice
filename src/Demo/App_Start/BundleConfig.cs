68e1da678ec7d25b4cd93d8b990e57677bc2ff79
joaofx <joaofx@gmail.com>
2015-01-09 20:44:45 -0200

    Added Bootstrap

---
+++ src/Demo/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo
+{
+    using System.Web.Optimization;
+
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Content/app.js")
+                .Include("~/Content/bower_components/jquery/dist/jquery.js")
+                .Include("~/Content/bower_components/bootstrap/dist/js/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/app.css")
+                .Include("~/Content/bower_components/bootstrap/dist/css/bootstrap.css"));
+        }
+    }
+}
